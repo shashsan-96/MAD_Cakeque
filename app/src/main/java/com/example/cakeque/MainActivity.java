34e6b7690497395a0bc39a235bf2f16b0d2d3e88
ShashiKrishan <vshashikrishan@yahoo.com>
2021-03-18 20:57:35 +0530

    Profile Dashboard UI finished page navigation set

--- app/src/main/java/com/example/cakeque/MainActivity.java
+++ app/src/main/java/com/example/cakeque/MainActivity.java
@@ -3,12 +3,27 @@ package com.example.cakeque;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.content.Intent;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    public Button buttonn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonn = (Button) findViewById(R.id.button);
+
+        buttonn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent= new Intent(MainActivity.this,ProfileDashboard.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/cakeque/ProfileDashboard.java
+++ app/src/main/java/com/example/cakeque/ProfileDashboard.java
@@ -3,12 +3,25 @@ package com.example.cakeque;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.content.Intent;
+import android.view.View;
 
 public class ProfileDashboard extends AppCompatActivity {
 
+    public Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_dashboard);
+
+        button = (Button) findViewById(R.id.btnCake);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent= new Intent(ProfileDashboard.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
