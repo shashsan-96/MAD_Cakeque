b338e3d10acde913b430e020b62512d482a007fa
ShashiKrishan <vshashikrishan@yahoo.com>
2021-03-19 01:59:19 +0530

    CreateProfile connected to Dashboard

--- app/src/main/java/com/example/cakeque/ProfileDashboard.java
+++ app/src/main/java/com/example/cakeque/ProfileDashboard.java
@@ -10,6 +10,7 @@ import android.view.View;
 public class ProfileDashboard extends AppCompatActivity {
 
     public Button button;
+    public Button button1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,6 +23,16 @@ public class ProfileDashboard extends AppCompatActivity {
                 Intent intent= new Intent(ProfileDashboard.this,MainActivity.class);
                 startActivity(intent);
             }
+
+        });
+        button1 = (Button) findViewById(R.id.btncen);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent1 = new Intent(ProfileDashboard.this,CreateProfile.class);
+                startActivity(intent1);
+            }
         });
     }
+
 }
