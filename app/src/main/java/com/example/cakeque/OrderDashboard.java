ac2348357f34bd2440513f2e99bd97b68d9817ba
NethmaSajani <ynsajani@gmail.com>
2021-03-19 15:08:26 +0530

    created order dashboard

---
+++ app/src/main/java/com/example/cakeque/OrderDashboard.java
@@ -0,0 +1,14 @@
+package com.example.cakeque;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OrderDashboard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order_dashboard);
+    }
+}
