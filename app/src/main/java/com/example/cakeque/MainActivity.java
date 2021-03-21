c7c53917eddbe49c49b12aad232feeba70604db7
NethmaSajani <ynsajani@gmail.com>
2021-03-21 12:55:42 +0530

    Finish order interfaces

--- app/src/main/java/com/example/cakeque/MainActivity.java
+++ app/src/main/java/com/example/cakeque/MainActivity.java
@@ -3,12 +3,27 @@ package com.example.cakeque;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.content.Intent;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    public Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.button2);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent= new Intent(MainActivity.this,OrderDashboard.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/cakeque/OrderDashboard.java
+++ app/src/main/java/com/example/cakeque/OrderDashboard.java
@@ -3,12 +3,60 @@ package com.example.cakeque;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.content.Intent;
+import android.view.View;
+
 
 public class OrderDashboard extends AppCompatActivity {
+    public Button button;
+    public Button button1;
+    public Button button2;
+    public Button cakebtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order_dashboard);
+
+        button = (Button) findViewById(R.id.button12);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent= new Intent(OrderDashboard.this,OrderList.class);
+                startActivity(intent);
+            }
+        });
+
+        button1 = (Button) findViewById(R.id.button15);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent= new Intent(OrderDashboard.this,CreateOrder.class);
+                startActivity(intent);
+            }
+        });
+
+        button2 = (Button) findViewById(R.id.button17);
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent= new Intent(OrderDashboard.this,ModifyOrder.class);
+                startActivity(intent);
+            }
+        });
+
+        cakebtn = (Button) findViewById(R.id.button3);
+
+        cakebtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent= new Intent(OrderDashboard.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
