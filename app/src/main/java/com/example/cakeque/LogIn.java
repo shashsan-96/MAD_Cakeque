480d91ce0505ce7004ffec0e3d4c8dec7ab65b56
ShashiKrishan <vshashikrishan@yahoo.com>
2021-05-02 15:08:01 +0530

    login page changed

--- app/src/main/java/com/example/cakeque/LogIn.java
+++ app/src/main/java/com/example/cakeque/LogIn.java
@@ -6,15 +6,19 @@ import android.os.Bundle;
 import android.widget.Button;
 import android.content.Intent;
 import android.view.View;
+import android.widget.EditText;
 
 public class LogIn extends AppCompatActivity {
     public Button button;
     public Button button1;
+    public Button button2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
+
+
 
         button = (Button) findViewById(R.id.btnReset1);
         button.setOnClickListener(new View.OnClickListener() {
@@ -31,6 +35,15 @@ public class LogIn extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent1 = new Intent(LogIn.this,SignUp.class);
                 startActivity(intent1);
+            }
+        });
+
+        button2 = (Button) findViewById(R.id.btnLogIn);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent2 = new Intent(LogIn.this,userPage.class);
+                startActivity(intent2);
             }
         });
     }
