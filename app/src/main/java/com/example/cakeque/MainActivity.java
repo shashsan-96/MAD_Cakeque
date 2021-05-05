3428c9d31b3780f910654f091a75648149a3d105
shashsan-96 <shashini.sandamini107@gmail.com>
2021-05-05 16:04:35 +0530

    create database connectivity for add function

--- app/src/main/java/com/example/cakeque/MainActivity.java
+++ app/src/main/java/com/example/cakeque/MainActivity.java
@@ -10,51 +10,44 @@ import android.view.View;
 public class MainActivity extends AppCompatActivity {
 
     public Button button;
+    public Button buttonn;
     public Button button1;
-    public Button button3;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        button =(Button) findViewById(R.id.h_propobtn);
+
+        button = (Button) findViewById(R.id.h_propobtn);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent= new Intent(MainActivity.this,proposal_dashboard.class);
+                Intent intent = new Intent(MainActivity.this, proposal_dashboard.class);
                 startActivity(intent);
             }
         });
 
-
-
+        buttonn = (Button) findViewById(R.id.button);
+        buttonn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent1 = new Intent(MainActivity.this, ProfileDashboard.class);
+                startActivity(intent1);
+            }
+        });
 
         button1 = (Button) findViewById(R.id.button2);
-
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent1= new Intent(MainActivity.this,OrderDashboard.class);
-                startActivity(intent1);
-
-
-            }
-
-        });
-
-
-
-
-
-        button3 = (Button) findViewById(R.id.button);
-
-        button3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent2= new Intent(MainActivity.this,ProfileDashboard.class);
+                Intent intent2 = new Intent(MainActivity.this, OrderDashboard.class);
                 startActivity(intent2);
             }
-
         });
+
     }
+
 }
+
+
--- app/src/main/java/com/example/cakeque/create_proposal.java
+++ app/src/main/java/com/example/cakeque/create_proposal.java
@@ -7,16 +7,53 @@ import android.widget.Button;
 import android.content.Intent;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class create_proposal extends AppCompatActivity {
     DatabaseHelper myDb;
     EditText edtuser,edtorder,edtproposition,edtcakecost,etddelivercost,edttotalcost,edtcontacts;
-    Button btnAdd;
+    Button submit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_proposal);
        myDb = new DatabaseHelper(this);
+
+        edtuser = (EditText) findViewById(R.id.userid);
+        edtorder = (EditText) findViewById(R.id.orderid);
+        edtproposition = (EditText) findViewById(R.id.proposition);
+        edtcakecost = (EditText) findViewById(R.id.cakecost);
+        etddelivercost = (EditText) findViewById(R.id.delivery);
+        edttotalcost = (EditText) findViewById(R.id.total);
+        edtcontacts = (EditText) findViewById(R.id.contact);
+        submit = (Button) findViewById(R.id.submit);
+        AddProposal();
+    }
+
+    public void AddProposal(){
+        submit.setOnClickListener(
+                new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+               boolean isInserted = myDb.insertData(edtuser.getText().toString() ,
+                        edtorder.getText().toString(),
+                        edtproposition.getText().toString(),
+                        edtcakecost.getText().toString(),
+                        etddelivercost.getText().toString(),
+                        edttotalcost.getText().toString(),
+                        edtcontacts.getText().toString()  );
+
+                if(isInserted = true)
+                    Toast.makeText(create_proposal.this,"Proposal Created",Toast.LENGTH_LONG).show();
+
+                else
+                    Toast.makeText(create_proposal.this,"Proposal Not Created",Toast.LENGTH_LONG).show();
+
+
+
+            }
+        });
     }
 }
