60db81c26e3b952af8bf97ed8ff898102a8ad472
shashsan-96 <shashini.sandamini107@gmail.com>
2021-05-05 15:05:08 +0530

    dbhelper created

--- app/src/main/java/com/example/cakeque/create_proposal.java
+++ app/src/main/java/com/example/cakeque/create_proposal.java
@@ -6,22 +6,17 @@ import android.os.Bundle;
 import android.widget.Button;
 import android.content.Intent;
 import android.view.View;
+import android.widget.EditText;
 
 public class create_proposal extends AppCompatActivity {
-
-    public Button button;
+    DatabaseHelper myDb;
+    EditText edtuser,edtorder,edtproposition,edtcakecost,etddelivercost,edttotalcost,edtcontacts;
+    Button btnAdd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_proposal);
-        button =(Button) findViewById(R.id.cakebtn3);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent= new Intent(create_proposal.this,MainActivity.class);
-                startActivity(intent);
-            }
-        });
+       myDb = new DatabaseHelper(this);
     }
 }
