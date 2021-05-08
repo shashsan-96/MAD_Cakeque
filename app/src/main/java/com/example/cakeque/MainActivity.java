d9402d87a6ab9a5bfab0d988116818b2d3e751e4
shashsan-96 <shashini.sandamini107@gmail.com>
2021-05-08 14:43:34 +0530

    calculation part done

--- app/src/main/java/com/example/cakeque/MainActivity.java
+++ app/src/main/java/com/example/cakeque/MainActivity.java
@@ -10,7 +10,7 @@ import android.view.View;
 public class MainActivity extends AppCompatActivity {
 
     public Button button;
-    public Button buttonn;
+
     public Button button1;
 
 
@@ -19,23 +19,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        button = (Button) findViewById(R.id.h_propobtn);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, proposal_dashboard.class);
-                startActivity(intent);
-            }
-        });
 
-        buttonn = (Button) findViewById(R.id.button);
-        buttonn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent1 = new Intent(MainActivity.this, ProfileDashboard.class);
-                startActivity(intent1);
-            }
-        });
+
+
 
         button1 = (Button) findViewById(R.id.button2);
         button1.setOnClickListener(new View.OnClickListener() {
@@ -43,6 +29,15 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent2 = new Intent(MainActivity.this, OrderDashboard.class);
                 startActivity(intent2);
+            }
+        });
+
+        button = (Button) findViewById(R.id.h_propobtn);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, proposal_dashboard.class);
+                startActivity(intent);
             }
         });
 
